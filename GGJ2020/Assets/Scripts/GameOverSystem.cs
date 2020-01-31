226448bfcec1be24fdf650e9125ff20f5ddad4b6
bariskes11 <kesdesng@gmail.com>
2020-01-31 19:43:26 +0300

    Gameover system

--- GGJ2020/Assets/Scripts/GameOverSystem.cs
+++ GGJ2020/Assets/Scripts/GameOverSystem.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverSystem : MonoBehaviour
 {
+
+    [SerializeField]
+    public GameObject gameoverPanel;
+    [SerializeField]
+    public GameObject restartButton;
+    
+
+    private void Awake()
+    {
+        
+        Button btn= restartButton.GetComponent<Button>();
+        btn.onClick.AddListener(() => RestartGame());
+        gameoverPanel.SetActive(false);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collider Çarptı" + collision.gameObject.tag);
@@ -11,6 +27,13 @@ public class GameOverSystem : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger Çarptı" + other.tag);
+        gameoverPanel.SetActive(true);
+      Time.timeScale = 0F;
+    }
+  public  void RestartGame()
+    {
+        int SceneIndex=SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(SceneIndex);
     }
 
 }
