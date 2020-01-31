25bd1f5f4cf95ec1fa0e3e4f54714844c63ce24d
bariskes11 <kesdesng@gmail.com>
2020-01-31 18:46:19 +0300

    Water System

---
+++ GGJ2020/Assets/Scripts/GameOverSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverSystem : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collider Çarptı" + collision.gameObject.tag);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Çarptı" + other.tag);
+    }
+
+}
---
+++ GGJ2020/Assets/Scripts/WaterSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSystem : MonoBehaviour
+{
+    [SerializeField]
+    GameObject waterObject;
+
+    public float floatSpeed;
+    public int OpenedDorCount;
+    public int UnRepairedFieldCount;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+
+        /// su Y ekseninde yukariya doğru ilerleyecek
+        
+        waterObject.transform.Translate(Vector3.up*Time.deltaTime*floatSpeed*OpenedDorCount*UnRepairedFieldCount);
+    }
+
+
+
+
+
+}
