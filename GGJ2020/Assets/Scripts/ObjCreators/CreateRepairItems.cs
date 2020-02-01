f4cf4d2d325eb3d58f70ebefef8f1bcee31f3bd2
bariskes11 <kesdesng@gmail.com>
2020-02-01 17:25:30 +0300

    General İmprovements

--- GGJ2020/Assets/Scripts/ObjCreators/CreateRepairItems.cs
+++ GGJ2020/Assets/Scripts/ObjCreators/CreateRepairItems.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateRepairItems: MonoBehaviour
+public class CreateRepairItems : MonoBehaviour
 {
     public float maxX;
     public float maxY;
@@ -10,9 +10,13 @@ public class CreateRepairItems: MonoBehaviour
     public GameObject brokenObject;
     public Vector3 maxVTR;
     public Vector3 minVTR;
+
+
+    
     public float minTime;
     public float maxtime;
     public float NextSpanTime;
+    public float wallRotation;
 
     private void Awake()
     {
@@ -29,13 +33,26 @@ public class CreateRepairItems: MonoBehaviour
         {
             yield return new WaitForSeconds(NextSpanTime);
             NextSpanTime = Random.Range(minTime, maxtime);
-            if (NextSpanTime == 0F)
-                NextSpanTime = 1F;
-            float ItemY = Random.Range(minVTR.y, maxVTR.y);
-            float Itemx = Random.Range(minVTR.x, maxVTR.x);
-            Vector3 position = new Vector3(Itemx, ItemY, minVTR.z);
-            Instantiate(brokenObject, position, Quaternion.identity);
+            float ItemY = Random.Range(0, 1F); // tüm komppantumanlar 0 ile 1 arası
+            StartCoroutine(TimeOutForWait());
+
+            float test_z = Random.Range(minVTR.z, maxVTR.z);
+            //float Itemx = Random.Range(minTransform.position.z, maxTrasnform.position.z);
+            Debug.Log(   " iTEM x" + test_z + "  Item Y"+ ItemY );
+            Vector3 position = new Vector3(0, ItemY, test_z);
+            GameObject gm = Instantiate(brokenObject, position, Quaternion.identity);
+            gm.transform.SetParent(transform);
+            Vector3 rt = new Vector3(0, wallRotation, 0);
+            gm.transform.Rotate(rt);
+            gm.transform.localPosition = new Vector3(0,ItemY, test_z);
         }
     }
 
+
+
+    IEnumerator TimeOutForWait()
+    {
+        yield return new WaitForSeconds(0.5F);
+
+    }
 }
