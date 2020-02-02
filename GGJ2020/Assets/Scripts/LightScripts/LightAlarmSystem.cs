a5016ed5d203356ea41954d60b78417c48b96caa
bariskes11 <kesdesng@gmail.com>
2020-02-02 06:45:44 +0300

    alartm system Compeleted

---
+++ GGJ2020/Assets/Scripts/LightScripts/LightAlarmSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightAlarmSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject AlarmLights;
+    
+    public string RoomName;
+    void Start()
+    {
+       GameObject g=  Instantiate(AlarmLights, transform);
+        AlarmLights = g;
+        g.SetActive(false);
+    }
+    private void Update()
+    {
+        GameObject rm = GameObject.Find(RoomName);
+        if (rm!=null)
+        {
+            AlarmLights.SetActive(true);
+        }
+        else
+        {
+            AlarmLights.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+
+}
